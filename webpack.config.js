8280a5b8fd49963b9622afa47be29efd91334e02
ahwei <aa9440307@gmail.com>
2019-01-23 12:46:02 +0800

    修改CSS DEV模式source map

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,44 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
+const isDevEnv = process.env.NODE_ENV === "development";
+console.log(process.env.NODE_ENV);
 module.exports = {
-  entry: [
-    './src/index.js',
-  ],
+  entry: ["./src/index.js"],
   output: {
-    path: __dirname + '/dist',
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js"
   },
   module: {
     rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: ["babel-loader"] },
       {
         test: /\.css/,
         use: [
-          'css-hot-loader',
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'resolve-url-loader'
-        ],
-      },
-      {
-        test: /\.(gif|jpe?g|png|woff|woff2|eot|ttf|svg)$/,
-        use: [
+          "css-hot-loader",
+          isDevEnv ? "style-loader" : MiniCssExtractPlugin.loader,
           {
-            loader: 'url-loader',
-          }
+            loader: "css-loader",
+            options: { sourceMap: isDevEnv }
+          },
+          "resolve-url-loader"
         ]
       },
       {
-        test: require.resolve('jquery'),
-        use: [{
-          loader: 'expose-loader',
-          options: 'jQuery'
-        }, {
-          loader: 'expose-loader',
-          options: '$'
-        }]
+        test: /\.(gif|jpe?g|png|woff|woff2|eot|ttf|svg)$/,
+        use: [{ loader: "url-loader" }]
+      },
+      {
+        test: require.resolve("jquery"),
+        use: [
+          { loader: "expose-loader", options: "jQuery" },
+          { loader: "expose-loader", options: "$" }
+        ]
       }
     ]
   },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
-  },
-
+  resolve: { extensions: ["*", ".js", ".jsx"] },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebPackPlugin({
@@ -63,16 +50,11 @@ module.exports = {
       chunkFilename: "[id].css"
     }),
     new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery',
-      Popper: ['popper.js', 'default'],
-    }),
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      Popper: ["popper.js", "default"]
+    })
   ],
-  devServer: {
-    historyApiFallback: true,
-    inline: true,
-    port: 8080,
-    hot: true,
-  }
+  devServer: { historyApiFallback: true, inline: true, port: 8008, hot: true }
 };
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,14 +1,14 @@
-const merge = require('webpack-merge');
-const config = require('./webpack.config.js');
-const webpack = require('webpack');
+const merge = require("webpack-merge");
+const config = require("./webpack.config.js");
+const webpack = require("webpack");
 
 module.exports = merge(config, {
-    mode: 'production',
-    plugins: [
-        new webpack.DefinePlugin({
-            "process.env": {
-                NODE_ENV: JSON.stringify("production")
-            }
-        })
-    ]
+  mode: "production",
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    })
+  ]
 });
